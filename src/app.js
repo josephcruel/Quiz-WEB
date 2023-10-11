65b0587b4168301dc86c12a1f2233c2a6bd3d06f
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-10-12 11:39:03 +1300

    all js stuff

--- src/app.js
+++ src/app.js
@@ -22,3 +22,60 @@ const cardsElement = []
 
 //Store card data
 const cardData = getCardData()
+
+function createCards() {
+    cardsData.forEach((data, index) => createCards(data, index)); //loop through the 
+}
+
+function createCard(data, index) { //Each cars will contain a question and answer
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    if (index === 0) {
+        card.classList.add('active');
+    }
+
+    card.innerHTML = `
+    <div class="inner-card">
+    <div class="inner-card-front">
+      <p>
+        ${data.question}
+      </p>
+    </div>
+      <div class="inner-card-back">
+      <p>
+      ${data.answer}
+      </p>
+    </div>
+    </div>
+    `;
+
+    card.addEventListener('click', () => card.classList.toggle('show-answer'));
+
+    //Add to DOM cards
+    cardsElement.push(card);
+
+    cardsContainer.appendChild(card); // Put in the container
+
+    updateCurrentText() 
+}
+
+// Show number of cards
+function updateCurrentText() {
+    currentElement.innerText = `${currentActiveCard + 1}/${cardsElement.length}`; // Add 1 to it because it's 0 by default
+}
+
+// Get cards from local storage
+function getCardData() {
+    //local storage only store string so we will take the array fetch back as an array by using parse
+    const cards = JSON.parse(localStorage.getItem('cards'));
+    return cards === null ? [] : cards; // if the cards are null return an empty array
+}
+
+// Add card to local storage
+function setCardData(cards) {
+    // Adding a card to the array and overwrite the array in storage
+    localStorage.setItem('cards', JSON.stringify(cards)); // We want to return it into a string
+    window.location.reload(); // To reflect on the DOM
+}
+
