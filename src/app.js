cf1a73f8cc10ad93b285101d91d385ed1a9b82c2
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-10-12 11:41:48 +1300

    forgot to save

--- src/app.js
+++ src/app.js
@@ -57,7 +57,7 @@ function createCard(data, index) { //Each cars will contain a question and answe
 
     cardsContainer.appendChild(card); // Put in the container
 
-    updateCurrentText() 
+    updateCurrentText();
 }
 
 // Show number of cards
@@ -79,3 +79,70 @@ function setCardData(cards) {
     window.location.reload(); // To reflect on the DOM
 }
 
+createCards();
+
+// Event listeners
+
+// Next Buttom 
+nextBtn.addEventListener('click', () => {
+    cardsElement[currentActiveCard].className = 'card left';
+
+    currentActiveCard = currentActiveCard + 1; // If we are at 1 it will 2 and so on 
+
+    // We need to set the index on the last card.
+    if (currentActiveCard > cardsElement.length - 1) {  // -1 because the array base is 0
+        currentActiveCard = cardsElement.length - 1; 
+    }
+
+    cardsElement[currentActiveCard].className = 'card active';  // Set the next card active by overwriting class name and gives the event   
+
+    updateCurrentText(); // To update the card numbers
+});
+
+// Previous Button
+prevBtn.addEventListener('click', () => {
+    currentElement[currentActiveCard].className = 'card right';
+
+    currentActiveCard = currentActiveCard - 1; // -1 for going back
+    
+    if (currentActiveCard < 0) {  // To prevent going pass 0
+      currentActiveCard = 0; // Set current to 0
+    }
+
+    cardsElement[currentActiveCard].className = 'card active';  // Set the next card active by overwriting class name and gives the event   
+
+    updateCurrentText(); // To update the card numbers
+});
+
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show')); // Add show class which will present out form ca
+// Hide add container 
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show')); // Add the hide cointainer CSS style.
+
+// Add new card
+addQuestionBtn.addEventListener('click', () => {
+    const question = questionElement.value; // Getting values from the form
+    const answer = answerElement.value; //Getting values from the form
+    console.log(question , answer)
+
+    if (question.trim() && answer.trim()) {
+        const newCard = { question, answer }; // Create a new card object
+
+        createCard(newCard);
+        
+        questionElement.value = ''; // Clear the form inputs
+        answerElement.value = ''; // Clear the form inputs
+
+        addContainer.classList.remove('show'); // Hide class container
+
+        cardsData.push(newCard); // Add new card to the array 
+        setCardsData(cardsData); // Passing to storage
+    }
+});
+
+// Clear cards button 
+clearBtn.addEventListener('click', () => {
+    localStorage.clear(); // Using clear method
+    cardsContainer.innerHTML = ''; // Take the cards out of the DOM
+    window.location.reload(); // Then reloadiong to update the page 
+})
